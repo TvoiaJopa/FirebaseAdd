b3b732b738f9eb9db82b3ac24e472f071b4bc728
Artemii Korshunov <pon4ickoff@yandex.ru>
2023-10-20 01:14:56 +0300

    init

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  firestore,
+  collection,
+  addDoc,
+  MESSAGES,
+  serverTimestamp,
+} from "./Config";
+import { useState } from "react";
 
 export default function App() {
+  const [newMessage, setNewMessage] = useState("");
+
+  const save = async () => {
+    const messageText = newMessage.toString(); // Convert to string
+    const docRef = await addDoc(collection(firestore, MESSAGES), {
+      text: messageText,
+      created: serverTimestamp(),
+    }).catch((error) => console.log(error));
+    setNewMessage("");
+    console.log("Message saved");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Send message..."
+        value={newMessage}
+        onChangeText={(text) => setNewMessage(text)} // Use onChangeText
+      />
+      <Button title="Send" type="button" onPress={save} />
     </View>
   );
 }
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
